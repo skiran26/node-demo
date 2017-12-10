ae57f25bc29771e94efaffbc8f2534c1d8d77ad0
techbrij <techbrij@gmail.com>
2017-12-10 17:43:16 +0530

    Updated to show connectionstring

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-app.get('/', (req, res) => res.send(process.env));
+app.get('/', (req, res) => res.send('App Setting: '+ process.env.APPSETTING_MyAppSettings + ' Connection String: ' + process.env.CUSTOMCONNSTR_MyConnectionString));
 app.listen (port, () => console.log('Server is running on port '+ port));
