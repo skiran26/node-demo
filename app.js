a60c95d1e29c4b9ece0a388c018194d1807b3d5a
techbrij <techbrij@gmail.com>
2017-12-10 17:00:13 +0530

    Updated for env variables

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const app = express();
-var port = process.env.PORT || 3000;
-app.get('/', (req, res) => res.send('Hello World !!'));
+const port = process.env.PORT || 3000;
+app.get('/', (req, res) => res.send(process.env));
 app.listen (port, () => console.log('Server is running on port '+ port));
